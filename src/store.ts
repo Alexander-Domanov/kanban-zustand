93e8222de2d6ecb8750cc5786f728d0a6be3f668
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-01 21:02:54 +0300

    feat(store): added reduxDevTools and localstorage

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,33 @@
 import {create} from "zustand";
+import {devtools, persist} from "zustand/middleware";
 
 const store = (set: any) => ({
-    tasks: [{title: 'TestTask', state: 'PLANNED'}],
+    tasks: [],
     draggedTask: null,
     addTask: (title: any, state: any) =>
-        set((store: any) => ({tasks: [...store.tasks, {title, state}]})),
+        set((store: any) => ({tasks: [...store.tasks, {title, state}]}),
+            false,
+            "addTask"
+        ),
     deleteTask: (title: any) =>
         set((store: any) => ({
-            tasks: store.tasks.filter((task: any) => task.title !== title),
-        })),
-    setDraggedTask: (title: any) => set({draggedTask: title}),
+                tasks: store.tasks.filter((task: any) => task.title !== title),
+            }),
+            false,
+            "deleteTask"
+        ),
+    setDraggedTask: (title: any) => set({draggedTask: title},
+        false,
+        "moveTask"
+    ),
     moveTask: (title: any, state: any) =>
         set((store: any) => (
-            {tasks: store.tasks.map((task: any) => task.title === title ? {title, state} : task)}
-        ))
+                {tasks: store.tasks.map((task: any) => task.title === title ? {title, state} : task)}
+            ),
+            false,
+            "moveTask"
+        )
 
 })
 
-export const useStore = create(store)
+export const useStore = create(persist(devtools(store), {name: "store"}))
