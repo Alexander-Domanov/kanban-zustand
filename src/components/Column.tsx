607c8ec574c6ebed703e36137d6627ba12ba7dc1
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-05-01 19:01:38 +0300

    feat(modal): added a modal window for adding tasks

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,12 @@
 import s from "./Column.module.css"
 import {Task} from "./Task";
 import {useStore} from "../store";
-
+import {useState} from "react";
 
 export const Column: React.FC<any> = ({state}) => {
+    const [text, setText] = useState('')
+    const [open, setOpen] = useState(false)
+
     const tasks = useStore((store) =>
         store.tasks.filter((task) => task.state === state))
     const addTask = useStore((store) => store.addTask)
@@ -12,11 +15,23 @@ export const Column: React.FC<any> = ({state}) => {
         <div className={s.column}>
             <div className={s.titleWrapper}>
                 <p>{state}</p>
-                <button onClick={() => addTask('TestTask ' + state, state)}>Add</button>
+                <button onClick={() => setOpen(true)}>Add</button>
             </div>
             {tasks.map((task: any, key) => (
                 <Task title={task.title} key={key}></Task>
             ))}
+            {open &&
+                <div className={s.Modal}>
+                    <div className={s.modalContent}>
+                        <input onChange={(e) => setText(e.target.value)} value={text}/>
+                        <button onClick={() => {
+                            addTask(text, state)
+                            setText('')
+                            setOpen(false)
+                        }}> Submit
+                        </button>
+                    </div>
+                </div>}
         </div>
     )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,7 +2,6 @@ import s from "./Task.module.css"
 import classNames from "classnames";
 import {useStore} from "../store";
 
-
 export const Task: React.FC<any> = ({title}) => {
     const task = useStore((store) =>
         store.tasks.find((task) => task.title === title)
